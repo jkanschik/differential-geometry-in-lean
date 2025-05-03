eb0667e93888b3edb27f85685811f656daec731b
Jens Lindekamp <jens.lindekamp@ista.com>
2025-05-03 19:48:00 +0200

    .

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = ['_build']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'karma-sphinx-theme'
+html_theme = 'karma_sphinx_theme'
 html_static_path = ['_static']
 
 # -- Options for LaTeX output ---------------------------------------------
